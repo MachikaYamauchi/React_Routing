ec1a61c1c6acc9406ca694ed45776270a3fa1bc0
Machika <machika111@gmail.com>
2022-12-02 13:36:38 -0800

    update files

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome"></Redirect>
+          </Route>
+          <Route path="/welcome">
+            <Welcome></Welcome>
+          </Route>
+          <Route path="/products" exact>{/* to direct the detail page, set exact */}
+            <Products></Products>
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail></ProductDetail>
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
